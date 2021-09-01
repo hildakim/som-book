a76b03a26390526556bc27a39ed7d335d5bef7af
chjs918 <zzisoo13@gmail.com>
2021-09-01 22:25:54 +0900

    base, chat

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import *
+from . import views
 
 app_name = "chat"
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('<str:room_name>/', views.room, name='room'),
 ]
