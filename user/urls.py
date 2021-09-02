c2b7631b5812befa5b6f45692416d7deed35a4a1
chjs918 <zzisoo13@gmail.com>
2021-09-02 20:16:00 +0900

    user

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,7 @@ from .views import *
 app_name = "user"
 
 urlpatterns = [
-
+    path('login/', login_view, name ="login"),
+    path('logout/', logout_view, name ="logout"),
+    path('register/', register_view, name = "signup"),
 ]
