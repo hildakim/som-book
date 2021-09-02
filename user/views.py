9d530e6e7527afd11c5c9e2889115984a15a7a09
yukhayoung <yukhayoung@gmail.com>
2021-09-02 23:49:10 +0900

    login fix

--- user/views.py
+++ user/views.py
@@ -13,7 +13,6 @@ def login_view(request):
             user = authenticate(request = request, username = username, password = password)
             if user is not None:
                 login(request, user)
-
         return redirect("community:community")
     else:
         form = AuthenticationForm()
