1625f2858e8a8a70dee86f095c61d0dc3de493d7
hildakim <pinkredjohn@gmail.com>
2021-09-04 20:25:01 +0900

    delete youtubelink

--- community/forms.py
+++ community/forms.py
@@ -4,14 +4,11 @@ from .models import *
 class CommunityForm(forms.ModelForm):
     class Meta:
         model = Community
-        fields = ['title','contents','image', 'youtube_link', 'tags']
+        fields = ['title','contents','image', 'tags']
 
         widgets = {
             'contents': forms.Textarea(
             attrs={'placeholder': '커뮤니티 글을 작성해주세요.'}),
-
-            'tags' : forms.TextInput(
-            attrs={'placeholder': '태그는 쉼표(,)로 구분됩니다.'}),
         }
 
 class BookmarkForm(forms.ModelForm):
